a16eaa0426119a68f2516fa9bfd326538273e03f
matteoforgia <matteo.forgiarini28@gmail.com>
2026-02-13 13:41:07 +0100

    completato il construttore di Forma, fare i vari metodi

--- src/ASCIICanvas.java
+++ src/ASCIICanvas.java
@@ -4,8 +4,16 @@ public class ASCIICanvas {
     private int larghezza; // Numero di "colonne"
     private int altezza; // Numero di "righe"
     // TODO Fai il costruttore
+    
+    public ASCIICanvas(int numeroForme, int larghezza, int altezza, Forma[] forme) {
+        this.numeroForme = numeroForme;
+        this.larghezza = larghezza;
+        this.altezza = altezza;
+        this.forme = new Forma[numeroForme];
+    }
     public void aggiungiForma(Forma f) {
     // TODO salva la forma nell'array
+        
     }
     public void rimuoviForma(int indice) {
     // TODO rimuovi la forma dall'array
