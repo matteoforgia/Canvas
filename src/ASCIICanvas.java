faf2c11dedeb349b8c4cc603e15122f5b03ae4bc
matteoforgia <matteo.forgiarini28@gmail.com>
2026-02-13 12:57:59 +0100

    Canvas

---
+++ src/ASCIICanvas.java
@@ -0,0 +1,16 @@
+public class ASCIICanvas {
+    private Forma[] forme;
+    private int numeroForme;
+    private int larghezza; // Numero di "colonne"
+    private int altezza; // Numero di "righe"
+    // TODO Fai il costruttore
+    public void aggiungiForma(Forma f) {
+    // TODO salva la forma nell'array
+    }
+    public void rimuoviForma(int indice) {
+    // TODO rimuovi la forma dall'array
+    }
+    public void disegna() {
+    // TODO implementa
+    }
+    }
